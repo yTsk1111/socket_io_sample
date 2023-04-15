e7968ea15c529fba6b04c9ba2ca0d22eb43fd1e8
yuki.tasaka <y.tasaka1111@gmail.com>
2023-04-15 22:43:32 +0900

    socket部分のモジュール化

--- app.js
+++ app.js
@@ -66,6 +66,9 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+const manipulate_socket = require('./socket');
+manipulate_socket(server)
+
 /**
  * Normalize a port into a number, string, or false.
  */
@@ -127,45 +130,47 @@ function onListening() {
 }
 
 
-let count = 0;
-let CLIENTS = []; // クライアントのリスト
-let id;
 
-const io = require("socket.io")(server);
-io.on('connection', (socket) => {
-  console.log('socket 接続成功')
-  id = Math.floor(Math.random() * 999999999);
-  console.log('新しいクライアント： ' + id);
-  const client = { id: id, socket: socket }
-  CLIENTS.push(client); //クライアントを登録
-  socket.send(count); // 現状のカウント情報を送信
-  socket.on('message', (message) => {
-    console.log('received: %s', message);
-    console.log(count)
-    if (message === 'kyosyu') {
-      count++;
-    }
-    else if (message === 'reset') {
-      count = 0
-    }
-    else {
-      console.log('received undefined message.')
-    }
-    socket.send(count);
-    for (let j = 0; j < CLIENTS.length; j++) {
-      //他の接続しているクライアントにメッセージを一斉送信
-      const saved_socket = CLIENTS[j]['socket']
-      if (socket !== saved_socket) { saved_socket.send(count); }
-    }
-  })
-  socket.on('close', () => {
-    console.log('ユーザー：' + id + ' がブラウザを閉じました');
-    for (let j = 0; j < CLIENTS.length; j++) {
-      const saved_id = CLIENTS[j]['id'];
-      if (id !== saved_id) { delete CLIENTS[id]; }
-    }
-  });
-})
 
+// let count = 0;
+// let CLIENTS = []; // クライアントのリスト
+
+// const io = require("socket.io")(server);
+// io.on('connection', (socket) => {
+//   console.log('socket 接続成功')
+//   const id = Math.floor(Math.random() * 999999999);
+//   console.log('新しいクライアント： ' + id);
+//   const client = { id: id, socket: socket }
+//   CLIENTS.push(client); //クライアントを登録
+//   socket.send(count); // 現状のカウント情報を送信
+
+//   socket.on('message', (message) => {
+//     console.log('received: %s', message);
+//     console.log(count)
+//     if (message === 'kyosyu') {
+//       count++;
+//     }
+//     else if (message === 'reset') {
+//       count = 0;
+//     }
+//     else {
+//       console.log('received undefined message.');
+//     }
+//     socket.send(count);
+//     for (let j = 0; j < CLIENTS.length; j++) {
+//       //他の接続しているクライアントにメッセージを一斉送信
+//       const saved_socket = CLIENTS[j]['socket']
+//       if (socket !== saved_socket) { saved_socket.send(count); }
+//     }
+//   })
+
+//   socket.on('close', () => {
+//     console.log('ユーザー：' + id + ' がブラウザを閉じました');
+//     for (let j = 0; j < CLIENTS.length; j++) {
+//       const saved_id = CLIENTS[j]['id'];
+//       if (id !== saved_id) { delete CLIENTS[id]; }
+//     }
+//   });
+// })
 
 module.exports = app;
